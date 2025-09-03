a99a757f54e3c644746807d12034f33fe49eaad5
Sukhovoy Alexey <a.sukhovoy@dpt-pro.ru>
2025-09-03 12:48:55 +0700

    Заготовка для будущей библеотеки

---
+++ src/lib/angular-maptalks.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-AngularMaptalks',
+  imports: [],
+  template: `
+    <p>
+      angular-maptalks works!
+    </p>
+  `,
+  styles: ``
+})
+export class AngularMaptalks {
+
+}
---
+++ src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of angular-maptalks
+ */
+
+export * from './lib/angular-maptalks';
