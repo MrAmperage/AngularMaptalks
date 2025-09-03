0626a4b52a71cd6cf74d056cce4f63b90a63fd0e
Sukhovoy Alexey <a.sukhovoy@dpt-pro.ru>
2025-09-03 16:47:17 +0700

    Мелкие фиксы карты

--- src/Modules/AngularMaptalksModule/Core/MapComponent/MapComponent.ts
+++ src/Modules/AngularMaptalksModule/Core/MapComponent/MapComponent.ts
@@ -1,9 +1,12 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  ElementRef,
   Input,
   OnInit,
+  ViewChild,
 } from "@angular/core";
+import { Map } from "maptalks";
 
 /*Компонент оболочка для Maptalks */
 @Component({
@@ -13,13 +16,18 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class MapComponent implements OnInit {
-  /*Отображать крест в центре карты */
+  /*Контейнер карты */
+  @ViewChild("Container", { static: true })
+  Container!: ElementRef<HTMLDivElement>;
+  /*Объект карты */
+  Map!: Map;
+  /*Отображать крест в центре карты? */
   @Input()
   CenterCross: boolean = false;
-  /*Режим плавного приближения*/
+  /*Режим плавного приближения?*/
   @Input()
   SeamlessZoom: boolean = true;
-  /*Фиксируемся в центре при приближении*/
+  /*Фиксируемся в центре при приближени?*/
   @Input()
   ZoomInCenter: boolean = false;
   InitMaptalks() {}
